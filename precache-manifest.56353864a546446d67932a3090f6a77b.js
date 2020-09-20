26cc42598c3c4b44fc9bb49c781387ce4feba337
Nico <eunico.cornelius@gmail.com>
2020-09-20 23:27:58 +0700

    Updates

---
+++ precache-manifest.56353864a546446d67932a3090f6a77b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab25d5ac04d4b89c1835c2a3e8de2ad",
+    "url": "/TicTacToe-React/index.html"
+  },
+  {
+    "revision": "00a1935c3367b852ed4e",
+    "url": "/TicTacToe-React/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "232994cd58b44d9306fe",
+    "url": "/TicTacToe-React/static/js/2.a1387eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe-React/static/js/2.a1387eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a1935c3367b852ed4e",
+    "url": "/TicTacToe-React/static/js/main.207d3ee1.chunk.js"
+  },
+  {
+    "revision": "5c8f6c44bec2322d6d96",
+    "url": "/TicTacToe-React/static/js/runtime-main.9406bb6a.js"
+  }
+]);
